772a350bed14169ec2d319c5a2ff5bb1813ba1ea
Tran Van Thanh (OUTSOURCE) <tranvanthanhtran.tt@gmail.com>
2022-07-08 09:55:20 +0700

    filter student pass

--- src/main/java/com/example/backend/api/ExcelController.java
+++ src/main/java/com/example/backend/api/ExcelController.java
@@ -1,22 +1,18 @@
 package com.example.backend.api;
 
-import com.example.backend.dto.StudentDTO;
 import com.example.backend.message.BaseResponse;
 import com.example.backend.service.ExcelService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("excel")
+@CrossOrigin
 public class ExcelController {
 
     private final ExcelService excelService;
--- src/main/java/com/example/backend/service/ExcelServiceImpl.java
+++ src/main/java/com/example/backend/service/ExcelServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.backend.service;
 
 import com.example.backend.dto.StudentDTO;
+import com.example.backend.mapper.StudentMapper;
+import com.example.backend.util.Constant;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.ss.usermodel.Cell;
@@ -8,14 +10,12 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -25,22 +25,103 @@ import java.util.List;
 @Slf4j
 public class ExcelServiceImpl implements ExcelService {
 
+    private final StudentService studentService;
+    private final StudentMapper studentMapper;
+
     @Override
     public String genStudentsPass(MultipartFile fileExcel) {
+        String result = "";
         try {
             // lấy danh sách học sinh trong file excel
-            List<StudentDTO> studentDTOSInput = getStudentsExcel(fileExcel);
-            System.out.println(studentDTOSInput);
+            List<StudentDTO> studentDTOSInput = getInputStudentsExcel(fileExcel);
             // lọc học sinh đạt
+            List<StudentDTO> studentDTOSPass = filterStudentPass(studentDTOSInput);
 
             // trả ra file excel danh sách học sinh đạt
+            result = getExcelNameStudentPass(studentDTOSPass);
         } catch (Exception e) {
             log.info(e.getMessage());
+            return result;
         }
+        return result;
+    }
+
+    private String getExcelNameStudentPass(List<StudentDTO> studentDTOSPass) {
         return null;
     }
 
-    private List<StudentDTO> getStudentsExcel(MultipartFile fileExcel) throws IOException {
+    private List<StudentDTO> filterStudentPass(List<StudentDTO> studentDTOSInput) {
+        List<StudentDTO> studentDTOSPass = new ArrayList<>();
+        for (StudentDTO studentDTO : studentDTOSInput) {
+            boolean isValidAge = validateAge(studentDTO);
+            if (!isValidAge) {
+                studentService.updateStudent(studentDTO);
+                continue;
+            }
+            boolean isValidNumberOfParticipation = validateNumberOfParticipation(studentDTO);
+            if (!isValidNumberOfParticipation) {
+                studentService.updateStudent(studentDTO);
+                continue;
+            }
+            boolean isValidConduct = validateConduct(studentDTO);
+            if (!isValidConduct) {
+                studentService.updateStudent(studentDTO);
+                continue;
+            }
+            boolean isValidAcademyAbility = validateAcademicAbility(studentDTO);
+            if (!isValidAcademyAbility) {
+                studentService.updateStudent(studentDTO);
+                continue;
+            }
+
+            studentDTO.setStatus(Constant.STATUS.PASS);
+            studentService.updateStudent(studentDTO);
+            studentDTOSPass.add(studentDTO);
+        }
+        return studentDTOSPass;
+    }
+
+    private boolean validateAcademicAbility(StudentDTO studentDTO) {
+        if (!Constant.RULE_STUDENT.HOC_LUC_GIOI.equalsIgnoreCase(studentDTO.getAcademicAbility())
+                || !Constant.RULE_STUDENT.HOC_LUC_KHA.equalsIgnoreCase(studentDTO.getAcademicAbility())) {
+            studentDTO.setStatus(Constant.STATUS.REJECT);
+            studentDTO.setReasonReject(Constant.REASON_REJECT.ACADEMIC_NOT_VALID);
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validateConduct(StudentDTO studentDTO) {
+        if (!Constant.RULE_STUDENT.HANH_KIEM_TOT.equalsIgnoreCase(studentDTO.getConduct())
+                || !Constant.RULE_STUDENT.HANH_KIEM_KHA.equalsIgnoreCase(studentDTO.getConduct())) {
+            studentDTO.setStatus(Constant.STATUS.REJECT);
+            studentDTO.setReasonReject(Constant.REASON_REJECT.CONDUCT_NOT_VALID);
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validateNumberOfParticipation(StudentDTO studentDTO) {
+        if (studentDTO.getNumberOfParticipation() >= 1) {
+            studentDTO.setStatus(Constant.STATUS.REJECT);
+            studentDTO.setReasonReject(Constant.REASON_REJECT.PARTICIPATION_NOT_VALID);
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validateAge(StudentDTO studentDTO) {
+        LocalDate localDate = LocalDate.now();
+        if (localDate.getYear() - studentDTO.getYearBirth() < 10
+                || localDate.getYear() - studentDTO.getYearBirth() > 16) {
+            studentDTO.setStatus(Constant.STATUS.REJECT);
+            studentDTO.setReasonReject(Constant.REASON_REJECT.AGE_NOT_VALID);
+            return false;
+        }
+        return true;
+    }
+
+    private List<StudentDTO> getInputStudentsExcel(MultipartFile fileExcel) throws IOException {
         InputStream inputStream = fileExcel.getInputStream();
         Workbook workbook = new XSSFWorkbook(inputStream);
         Sheet sheet = workbook.getSheet("DS ĐT đăng ký cho con em");
